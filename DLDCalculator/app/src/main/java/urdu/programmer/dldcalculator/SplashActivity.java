99d0b17e03f5db9173d9a1aa7bac39eafc26255e
im-umair-ali <muhammadumairali525@gmail.com>
2022-07-14 12:34:53 +0500

    Completed

---
+++ DLDCalculator/app/src/main/java/urdu/programmer/dldcalculator/SplashActivity.java
@@ -0,0 +1,14 @@
+package urdu.programmer.dldcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
